107b6ede940850f3451acd6664e7d46de6c92db5
hhjo <43205471+hhjo96@users.noreply.github.com>
2025-12-12 20:06:04 +0900

    Style: Characters target -> Characters ch

--- src/runningClass/HW2/Characters.java
+++ src/runningClass/HW2/Characters.java
@@ -25,7 +25,7 @@ public abstract class Characters {
         characterCount++;
     }
 
-    abstract void ult(Characters target);
+    abstract void ult(Characters ch);
     abstract void ownSkill();
 
     void fillHealth(int health) {
